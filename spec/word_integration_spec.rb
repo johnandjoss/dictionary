01e25de48f90165db20b9e2d7df53230bee42e31
Guest User <student@epicodus.com>
2015-05-01 12:04:09 -0700

    describe and sets path for form for adding new definitions to a specific word; web app displays and collects information from form correctly; capybara testing passes all current specs

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -19,9 +19,27 @@ describe('path for words/new', {:type => :feature}) do
     end
   end
 
-  describe('path for adding/showing all definitions for a word', {:type => :feature}) do
-      it('allows user to see current definitions and links to') do
-        visit('/words/1')
-        expect(page).to have_content('Here are all')
+describe('path for showing all definitions for a word', {:type => :feature}) do
+    it('allows user to see current definitions and links to') do
+      visit('/words/1')
+      expect(page).to have_content('Here are all')
     end
+  end
+
+describe('path to get to form to add new definitions for a word', {:type => :feature}) do
+    it('allows user to click link to bring up form to add new definitions') do
+      visit('/words/1')
+      click_link('Add a new definition')
+      expect(page).to have_content('Add a')
     end
+end
+
+describe('path for adding new definitions for a word', {:type => :feature}) do
+    it('allows user input another set of definitions for a specific word') do
+      visit('/words/1/definitions/new')
+      fill_in('def_one', :with => 'tall frog')
+      fill_in('def_two', :with => 'big toad')
+      click_button('Add Definition')
+      expect(page).to have_content('Your item has been')
+    end
+end
