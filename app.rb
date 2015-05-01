df2cad47e816b9f9f255bea2dafda07766b12836
Guest User <student@epicodus.com>
2015-05-01 11:50:00 -0700

    add path to show all definitions for a single word and has link to add additional definitions; capybarra tests pass; added additional view files needed to support path

--- app.rb
+++ app.rb
@@ -21,3 +21,23 @@ post('/words') do
   @words = Word.all()
   erb(:success)
 end
+
+get('/words/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word)
+end
+
+get('/words/:id/definitions/new') do
+    @word = Word.find(params.fetch('id').to_i())
+    erb(:word_definitions_form)
+end
+
+post('/definitions') do
+  def_one = params.fetch('def_one')
+  def_two = params.fetch('def_two')
+  @definition = Definition.new(def_one, def_two)
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
+  erb(:success)
+end
--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -18,3 +18,10 @@ describe('path for words/new', {:type => :feature}) do
       expect(page).to have_content('Your item has been')
     end
   end
+
+  describe('path for adding/showing all definitions for a word', {:type => :feature}) do
+      it('allows user to see current definitions and links to') do
+        visit('/words/1')
+        expect(page).to have_content('Here are all')
+    end
+    end
