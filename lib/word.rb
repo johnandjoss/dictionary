71cb08b9da76e1de52655adbf0b0508f524bcfd3
Guest User <student@epicodus.com>
2015-05-01 10:14:11 -0700

    pass rspec testing for .all, .clear and #save methods for Word class

--- lib/word.rb
+++ lib/word.rb
@@ -9,4 +9,15 @@ class Word
     @id = @@words.length().+(1)
   end
 
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
+  end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,11 +3,34 @@ require('word')
 
 
 describe(Word) do
+  before() do
+    Word.clear()
+  end
 
   describe('#name') do
     it('returns the name of the word') do
       test_word = Word.new("sofa")
       expect(test_word.name()).to(eq("sofa"))
+    end
+  end
+
+  describe('#save') do
+    it('adds a word to the array of saved words') do
+      test_word = Word.new("book")
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+  describe('.all') do
+    it('is empty at first') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+  describe('.clear') do
+    it('empties out all of the saved definitions') do
+      Word.new('cloud').save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
     end
   end
 
